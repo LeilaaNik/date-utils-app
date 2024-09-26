292d0208a7762bf29ee2d114449a9b31286e6329
Leila Nikkheslat <ln223kd@student.lnu.se>
2024-09-26 23:22:09 +0200

    Normalize all line endings

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * The main script file for the date-time module.
+ *
+ * @version 1.0.0
+ */
+
+// Import the functions from the modules
+const formatDate = require('./formatDate')
+
+
+module.exports = {
+  formatDate
+}
