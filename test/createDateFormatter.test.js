7d87462f44bdfc35f0f51a3659763b90c3889662
Leila Nikkheslat <ln223kd@student.lnu.se>
2024-09-28 08:52:46 +0200

    Update test code to use createDateFormatter instead of formatDate

--- test/createDateFormatter.test.js
+++ test/createDateFormatter.test.js
@@ -1,7 +1,8 @@
-const formatDate = require('../src/formatDate')
+const createDateFormatter = require('../src/createDateFormatter')
 
 test('formats date correctly', () => {
+  const formatDate = createDateFormatter()
   const date = new Date('2023-10-01')
-  const formattedDate = formatDate(date, 'YYYY-MM-DD')
+  const formattedDate = formatDate(date)
   expect(formattedDate).toBe('2023-10-01')
 })
