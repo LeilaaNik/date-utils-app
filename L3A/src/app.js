02eeb4c7c7d38b75184e1491c8bdcf30b947bed7
Leila Nikkheslat <ln223kd@student.lnu.se>
2024-10-23 12:45:40 +0200

    Add entry point for Date-Time Utility App

---
+++ L3A/src/app.js
@@ -0,0 +1,8 @@
+const DateTimeController = require('./controllers/DateTimeController');
+
+/**
+ * Entry point for the Date-Time Utility App.
+ * Initializes and runs the DateTimeController.
+ */
+const app = new DateTimeController();
+app.run();
