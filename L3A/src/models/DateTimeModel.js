f9da500e37eb47593f5003a8901a337f7f79f9e0
Leila Nikkheslat <ln223kd@student.lnu.se>
2024-10-23 12:28:46 +0200

    Create DateTimeModel file in the models folder and import utility functions from the L2M directory

---
+++ L3A/src/models/DateTimeModel.js
@@ -0,0 +1,5 @@
+const addDays = require('../../../L2M/addDays');
+const createDateFormatter = require('../../../L2M/createDateFormatter');
+const dateDifference = require('../../../L2M/dateDifference');
+const isWeekend = require('../../../L2M/isWeekend');
+const parseDate = require('../../../L2M/parseDate');
